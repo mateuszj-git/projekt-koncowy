b10fe5476bf3f12a0039eae4be785f9a4d909355
Mateusz <mateusz.jasek6@gmail.com>
2021-06-15 23:41:33 +0200

    comit

--- src/components/Main.js
+++ src/components/Main.js
@@ -158,7 +158,7 @@ export default class Main {
                 this1.updateFloor(floor1)
             })
 
-        }, 100);
+        }, 60);
         setInterval(function () {
             socket.emit('position', position)
             socket.on('sendposition', position1 => {
@@ -166,7 +166,7 @@ export default class Main {
 
 
             })
-        }, 100);
+        }, 60);
         setInterval(function () {
             socket.emit('points', pkt)
             socket.on('sendpoints', pktp1 => {
